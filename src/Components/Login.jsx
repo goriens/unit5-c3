297d3324fc0b53a780fe60fb1085e257c8a37a6c
Suhaib Gour <76823175+goriens@users.noreply.github.com>
2022-04-25 18:16:13 +0530

    Update Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { AuthContext } from "./Context/AuthContext";
+import { AuthContext } from "../Context/AuthContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
